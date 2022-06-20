40008936a9213e1df965c6af5b92512ad84b4bda
seen-at <90022384+seen-at@users.noreply.github.com>
2022-06-20 17:40:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ let hasBlackJack = false;
 let isAlive = false;
 let message = "";
 
+// capturing all the contents in the page 
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el");
 let playerEl = document.getElementById("player-el")
 
+// displaying the player object in the <p> section of the html
 playerEl.textContent = `${player.name} : \$${player.chips}`;
 
 function getRandomNumber() {
@@ -26,6 +28,8 @@ function getRandomNumber() {
 	}
 }
 
+// startGame() function in the Start Game put into effect
+// get two random values of cards and stores them in the array
 function startGame() {
 	isAlive = true;
 	let firstCard = getRandomNumber();
@@ -35,7 +39,10 @@ function startGame() {
 	renderGame();
 }
 
+// renderGame() which displays the values of cards generated in startGame()
+// dispays the message for different values of sum
 function renderGame() {
+	// adds the numbers from the array beside Cards: string	
 	cardsEl.textContent = "Cards: "
 	for (let i = 0; i < cards.length; i += 1) {
 		cardsEl.textContent += cards[i] + " "
@@ -52,9 +59,11 @@ function renderGame() {
 		message = "You are out of the game!"
 		isAlive = false;
 	}
+	// captures message from if--else and converts the message-el in html to the captured message
 	messageEl.textContent = message
 }
 
+// new random number generated only if certain conditions are fulfilled, then added to the array
 function newCard() {
 	if (isAlive === true && hasBlackJack === false) {
 		let newCard = getRandomNumber();
